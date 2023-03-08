4d5e48cef8167934ccebce0de69a982726d3092f
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2023-03-07 21:22:33 -0300

    Update conversor.js

--- conversor.js
+++ conversor.js
@@ -1,9 +1,9 @@
 function bin_to_decimal(bin) {
-    var weight = 1;
+    var weight = 0;
     var decimal = 0;
-    for(var i = 1; i < bin.length; i++) {
-        weight += weight;
-        console.log(weight)
+    
+    for(var i = bin.length - 1; i >= 0; i--) {
+        weight += i < bin.length - 1 ? weight : 1;
         if (bin[i] !== 0 || bin[i] == true) {
             decimal += weight;
         }
@@ -14,5 +14,18 @@ function bin_to_decimal(bin) {
 function decimal_to_bin(decimal) {
     
 }
+/*
+ * testes 
+**/
 
-console.log(bin_to_decimal([1,0,0,1]))
+console.log(bin_to_decimal([0,1,0,1]))
+
+console.log(bin_to_decimal([1,1,1,1]))
+
+console.log(bin_to_decimal([1,0,1,0]))
+
+console.log(bin_to_decimal([0,0,1,0,1,1,0,1]))
+
+console.log(bin_to_decimal("0001 1100 1001".replaceAll(' ', '').split('')))
+
+console.log(bin_to_decimal([0,0,0,1,1,1,0,0,1,0,0,1]))
